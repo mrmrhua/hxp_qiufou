8301884e5fcd230308a3693bcc17907ab1107b1f
DingYH <mrmrhua@126.com>
2017-09-05 14:05:53 +0800

    no message

--- tesing/fabfile.py
+++ tesing/fabfile.py
@@ -1,16 +1,22 @@
 from fabric.api import run,env,cd,prefix
 
-env.hosts =['123.206.188.234']
+env.hosts =['115.159.115.96']
 env.user = 'root'
 env.password='qclouding123'
 env.activate = 'source venv/bin/activate'
 
+# def go():
+#     with cd('/home/hxp'):
+#         with prefix(env.activate):
+#             run('')
 def go():
-    with cd('/home/wwwroot/houxiaopang.com'):
-        with prefix(env.activate):
-            run('gunicorn manage:app -p manage.pid -b 127.0.0.1:8000 -D')
-
+    run('systemctl start hxp')
 
 def stop():
-    with cd('/home/wwwroot/houxiaopang.com'):
-        run('kill `cat manage.pid`')
+    run('systemctl stop')
+
+def status():
+    run('systemctl status hxp')
+
+def restart():
+    run('systemctl restart hxp')
