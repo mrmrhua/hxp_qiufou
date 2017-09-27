1e2eee046c8351802d2fc4c78c9b2446ffbca817
DingYH <mrmrhua@126.com>
2017-09-27 15:22:34 +0800

    no message

--- wx/config/dev.py
+++ wx/config/dev.py
@@ -2,4 +2,4 @@ ENVFLAG='dev'
 DEBUG = True
     #
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dyh:baiduding123@mysql.rdsmbhsvs1vn5cx.rds.gz.baidubce.com/wxhxp'
-
+# SQLALCHEMY_DATABASE_URI ='mysql+pymysql://dev:qclouding123@10.66.184.178/wxhxp'
--- wx/config/production.py
+++ wx/config/production.py
@@ -1,3 +1,4 @@
 ENVFLAG = 'production'
 DEBUG = False
-SQLALCHEMY_DATABASE_URI ='mysql+pymysql://dev:qclouding123@10.66.184.178/wxhxp'
+# SQLALCHEMY_DATABASE_URI ='mysql+pymysql://dev:qclouding123@10.66.184.178/wxhxp'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dyh:baiduding123@mysql.rdsmbhsvs1vn5cx.rds.gz.baidubce.com/wxhxp'
