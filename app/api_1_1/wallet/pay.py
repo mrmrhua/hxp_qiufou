61822239f554625b31697a1466ca37c7166afd21
DingYH <mrmrhua@126.com>
2017-11-16 14:44:42 +0800

    no message

--- app/api_1_1/wallet/pay.py
+++ app/api_1_1/wallet/pay.py
@@ -94,7 +94,10 @@ class GetAlipayCharge(Resource):
         body = request.values.get("body")
         try:
             pingpp.api_key = live_key
-            pingpp.private_key_path = '/Users/ding/dev/hxp/pingxxprivate.pem'
+            if current_app.debug:
+                pingpp.private_key_path = '/Users/ding/dev/hxp/pingxxprivate.pem'
+            else:
+                pingpp.private_key_path = '/home/hxp/pingxxprivate.pem'
             charge = pingpp.Charge.create(
                 order_no=order_no,  # 推荐使用 8-20 位，要求数字或字母，不允许其他字符 HXP100000+id
                 amount=amount,  # 订单总金额, 人民币单位：分（如订单总金额为 1 元，此处请填 100）
@@ -104,19 +107,22 @@ class GetAlipayCharge(Resource):
                 client_ip=ip,  # 发起支付请求客户端的 IP 地址，格式为 IPV4，如: 127.0.0.1
                 subject=subject,
                 body=body,
-                # extra=dict(
-                #     # 必须，支付成功的回调地址，在本地测试不要写 localhost ，请写 127.0.0.1。URL 后面不要加自定义参数。
-                #     success_url='127.0.0.1',
-                #     # 可选，支付取消的回调地址， app_pay 为true时，该字段无效，在本地测试不要写 localhost ，请写 127.0.0.1。URL 后面不要加自定义参数。
-                #     # cancel_url='http://example.com/cancel',
-                #     # 可选，2016 年 6 月 16 日之前登录 Ping++ 管理平台填写支付宝手机网站的渠道参数的旧接口商户，需要更新接口时设置此参数值为true，6月16号后接入的新接口商户不需要设置该参数。
-                #     # new_version=True,
-                #     # 可选，是否使用支付宝客户端支付，该参数为true时，调用客户端支付。
-                #     # app_pay=True,
-                # )
+                extra=dict(
+                    # 必须，支付成功的回调地址，在本地测试不要写 localhost ，请写 127.0.0.1。URL 后面不要加自定义参数。
+                    success_url='http://192.168.0.101:8080/#/prompt',
+                    # 可选，支付取消的回调地址， app_pay 为true时，该字段无效，在本地测试不要写 localhost ，请写 127.0.0.1。URL 后面不要加自定义参数。
+                    # cancel_url='http://example.com/cancel',
+                    # 可选，2016 年 6 月 16 日之前登录 Ping++ 管理平台填写支付宝手机网站的渠道参数的旧接口商户，需要更新接口时设置此参数值为true，6月16号后接入的新接口商户不需要设置该参数。
+                    # new_version=True,
+                    # 可选，是否使用支付宝客户端支付，该参数为true时，调用客户端支付。
+                    app_pay=True,
+                )
             )
+            print(ip)
+            print(charge)
             return jsonify({"code": 0,"data":{"charge":charge}})
         except Exception as e:
+            print(e)
             return jsonify({"code": -1})
 
 
