f7a928204a6ce56adc820730ef8cfce75ea66bfe
DingYH <mrmrhua@126.com>
2017-11-17 11:43:09 +0800

    no message

--- config/production.py
+++ config/production.py
@@ -1,4 +1,4 @@
 ENVFLAG = 'production'
 DEBUG = False
-# SQLALCHEMY_DATABASE_URI ='mysql+pymysql://dev:qclouding123@10.66.184.178/hxp'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dyh:baiduding123@mysql.rdsmbhsvs1vn5cx.rds.gz.baidubce.com/hxp'
+SQLALCHEMY_DATABASE_URI ='mysql+pymysql://dev:qclouding123@10.66.184.178/hxp'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dyh:baiduding123@mysql.rdsmbhsvs1vn5cx.rds.gz.baidubce.com/hxp'
