04bce4a2a7ca58c74f255bbc28614a796275be43
lizhiwei <617092800@qq.com>
2017-11-30 17:22:17 +0800

    作品集修改

--- src/workdetail/src/components/WeChat.vue
+++ src/workdetail/src/components/WeChat.vue
@@ -1,41 +1,76 @@
 <template>
-  <div>
+  <div style="background: #f5f5f5">
     <div class="info">
       <div class="fg"></div>
     </div>
-    <div class="userinfo">
+    <div class="userinfo" style="margin-top: 120px">
       <div class="head_div"><img :src="userinfo.headimg"></div>
       <p v-html="userinfo.nickname"></p>
-      <p><i></i><span v-text="setcity(userinfo.city)"></span>|<span v-html="setexp(userinfo.exp)"></span></p>
+      <!--所在城市&工作经验-->
+      <div class="cityexp_box">
+        <p><i></i><span v-text="setcity(userinfo.city)"></span></p>
+        <p style="margin-left: 150px"><i></i><span v-html="setexp(userinfo.exp)" ></span></p>
+      </div>
+      <!--风格标签-->
+      <div class="stylebox">
+        <ul>
+          <li><div>扁平风</div></li>
+          <li><div>青春活泼</div></li>
+          <li><div>大客户经验</div></li>
+          <li><div>大客户经验</div></li>
+          <li><div>扁平风</div></li>
+          <li><div>大客户经验</div></li>
+          <li><div>大客户经验</div></li>
+          <li><div>大客户经验</div></li>
+          <li><div>大客户经验</div></li>
+        </ul>
+      </div>
       <!--擅长-->
       <div class="sc" v-show="userinfo.filed.length>0">
-        <span style="float: left;">擅长领域：</span>
-        <ul style="float: right;width: 313px;padding-top: 10px">
+        <span class="span">擅长领域</span>
+        <ul style="width: 800px;padding-top: 10px;margin: 0 auto;">
           <li v-for="item in userinfo.filed" v-html="item"></li>
         </ul>
       </div>
-      <div v-show="userinfo.worktime.length>0" class="worktime" style="overflow: hidden;"><span>可工作时间：</span>
-        <ul style="float: right;width: 313px;color: #313131;line-height: 28px;margin-top: 6px;">
+      <div v-show="userinfo.worktime.length>0" class="worktime" >
+        <span class="span">可工作时间：</span>
+        <ul style="width: 800px;padding-top: 10px;margin: 0 auto;">
           <li v-for="item in userinfo.worktime" v-html="formatWorktime(item)"></li>
         </ul>
       </div>
+    </div>
+
+    <!--个人简历-->
+    <div  class="userinfo" v-show="userinfo.intro">
+      <div  style="overflow: hidden;margin-top: 20px;">
+        <span class="span" >个人简介</span>
+        <div style="width: 660px;;margin: 20px auto;text-align:left;padding: 14px" v-html="settext(userinfo.intro)">
+        </div>
+      </div>
+    </div>
       <!--项目经历-->
-      <div v-show="userinfo.project_text.length>0" style="overflow: hidden;margin-top: 20px;">
-        <span style="float: left;">项目经历：</span>
+    <div  class="userinfo" v-show="userinfo.project_text.length>0">
+      <div  style="overflow: hidden;margin-top: 20px;">
+        <span class="span" >项目经历</span>
         <ul id="project_text"
-            style="float: right;width: 313px;line-height: 20px;margin-top: 10px;height: 62px;overflow: hidden;">
+            style="width: 800px;line-height: 20px;margin:0 auto;overflow: hidden;">
           <li v-for="item in userinfo.project_text">
-            <p style="font-weight: 600;" v-html="item.title"></p>
-            <p style="margin-bottom: 40px;" v-html="item.desc"></p>
+            <div style="width: 660px;border: 1px solid #D9D9D9;margin: 20px auto;text-align: left;padding: 14px">
+              <p style="font-weight: 600;" v-html="item.title"></p>
+              <p style="margin-bottom: 10px;" v-html="item.desc"></p>
+            </div>
+
           </li>
         </ul>
       </div>
-      <div v-show="userinfo.project_text.length>0" @click="showmore" class="more"><i
-        class="iconfontyyy">&#xe8f8;</i><span>展开</span>
-      </div>
+      <!--<div v-show="userinfo.project_text.length>0" @click="showmore" class="more">-->
+        <!--<i class="iconfontyyy">&#xe8f8;</i>-->
+        <!--<span>展开</span>-->
+      <!--</div>-->
     </div>
     <div class="main">
-      <div style="height: 80px;background: #fff;border-top: 1px solid #dedede;">
+      <div style="height: 115px;background: #fff;width: 1100px;margin: 0 auto;text-align: center;padding-top: 35px;border-bottom: 1px solid #dedede;">
+        <span class="span" >作品案例</span>
         <ul id="category" class="category">
           <li class="active"><a>全部</a></li>
           <li><a>PPT定制</a></li>
@@ -48,7 +83,7 @@
       </div>
       <div class="content">
         <!--显示的品类-->
-        <ul style="overflow: hidden">
+        <ul style="overflow: hidden;">
           <li id="imgwarp" v-for="itme in albums">
             <a target="_blank" :href="'http://houxiaopang.com/workdetail/album/'+itme.work_id">
               <img :data-original="itme.cover"  class="img">
@@ -159,7 +194,13 @@
           return (y - exp) + "年工作经验";
         }
       },
-      showmore(){
+      settext(value){
+        if (value) {
+          return value.replace(/\r\n/g, "<br>").replace(/\n/g, "<br>");
+        }
+        return value
+      },
+/*      showmore(){
         this.more = !this.more;
         if (this.more) {
           $("#project_text").css("height", "auto");
@@ -171,7 +212,7 @@
           $(".userinfo .more span").eq(0).html("展开");
           window.scrollTo(0, 600);
         }
-      },
+      },*/
       /*jump(data){
        var id = data.id;
        if (data.name === "project") {
@@ -216,6 +257,7 @@
               if (data.data.worktime == null || data.data.worktime == "") {
                 data.data.worktime = []
               }
+              data.data.worktime = JSON.parse(data.data.worktime)
               that.userinfo = data.data;
             } else {
               alert(data.data.msg||"网络拥挤，请稍后再试···");
@@ -305,7 +347,11 @@
   .info {
     background: url('http://image.houxiaopang.com/workdetail/bg.png') no-repeat;
     background-size: cover;
+    width: 100%;
     height: 340px;
+    position: absolute;
+    top: 90px;
+    z-index: 0;
   }
 
   .fg {
@@ -316,6 +362,12 @@
     height: 100%;
   }
 
+  .span{
+    font-size:18px;
+    color:#D01667;
+    line-height:19px;
+  }
+
   ul {
     list-style: none;
   }
@@ -324,14 +376,18 @@
     color: #313131;
   }
   .userinfo {
-    width: 600px;
-    margin: 0 auto;
-    z-index: 10;
+    width: 1100px;
+    margin: 20px auto;
     box-sizing: border-box;
     color: #727272;
     line-height: 40px;
     padding: 15px 60px 50px;
+    background: #ffffff;
+    text-align: center;
+    position: relative;
+    z-index: 0;
   }
+
 
   .userinfo > .head_div {
     margin: -70px auto 0 auto;
@@ -357,35 +413,49 @@
     height: 50px;
     line-height: 50px;
     color: #313131;
-
   }
 
-  .userinfo > p:nth-of-type(2) {
+  .cityexp_box{
     text-align: center;
     color: #999;
     height: 30px;
     line-height: 30px;
     font-size: 16px;
+
+  }
+  .cityexp_box > p{
+    display: inline-block;
     position: relative;
   }
 
-  .userinfo > p:nth-of-type(2) > span:nth-child(2) {
-    padding-right: 15px;
-    padding-left: 20px;
-  }
 
-  .userinfo > p:nth-of-type(2) > span:nth-child(3) {
-    padding-left: 15px;
-  }
-
-  .userinfo > p > i {
+  .cityexp_box > p > i {
     background: url('http://image.houxiaopang.com/workdetail/dingwei.png') no-repeat;
-    display: inline-block;
     width: 12px;
     height: 17px;
     background-size: cover;
     position: absolute;
     top: 6px;
+    left: -20px;
+  }
+  .stylebox{
+    text-align: center;
+    width: 800px;
+    margin: 30px auto;
+    overflow: hidden;
+  }
+  .stylebox >ul{
+    width: 100%;
+  }
+  .stylebox >ul >li{
+    display: inline-block;
+    margin-right: 14px;
+    margin-bottom: 10px;
+  }
+  .stylebox >ul >li >div{
+    border: 1px solid #d9d9d9;
+    border-radius: 100px;
+    padding:0 25px;
   }
 
   .main {
@@ -394,21 +464,40 @@
 
   .sc {
     overflow: hidden;
-    margin-top: 20px;
+    margin:30px 0;
+    text-align: center;
   }
 
-  .userinfo > .sc li {
-    float: left;
+  .userinfo > .sc li{
+    display: inline-block;
     padding: 0 25px;
-    border-radius: 25px;
     text-align: center;
-    height: 30px;
-    line-height: 28px;
+    height: 40px;
+    line-height: 38px;
     margin-right: 20px;
     margin-bottom: 10px;
-    background: #eee;
+    background: #f5f5f5;
     color: #828282;
   }
+
+  .worktime {
+    overflow: hidden;
+    margin:30px 0;
+    text-align: center;
+  }
+
+  .userinfo > .worktime li{
+    display: inline-block;
+    padding: 0 25px;
+    text-align: center;
+    height: 40px;
+    line-height: 38px;
+    margin-right: 20px;
+    margin-bottom: 10px;
+    background: #f5f5f5;
+    color: #828282;
+  }
+
 
   .userinfo > .more {
     width: 60px;
@@ -429,14 +518,17 @@
   .content {
     box-sizing: border-box;
     width: 1100px;
-    margin: 20px auto 0;
+    margin: 0 auto;
     height: 100%;
+    background: #ffffff;
   }
 
   .content > ul {
     overflow: hidden;
     box-sizing: border-box;
     list-style: none;
+    width: 821px;
+    margin: 0 auto;
   }
 
   .content > ul > li {
@@ -444,15 +536,15 @@
     height: 295px;
     float: left;
     box-sizing: border-box;
-    margin-right: 19px;
+    margin-top: 29px;
     margin-bottom: 25px;
     overflow: hidden;
     background: #fff;
     box-shadow: 1px 1px 5px 0 #e9dede;
   }
 
-  .content > ul > li:nth-child(4n) {
-    margin-right: 0;
+  .content > ul > li:nth-child(3n+2) {
+    margin: 29px 20px 25px 21px;
   }
 
   .content > ul > li:hover {
@@ -511,7 +603,7 @@
   }
 
   .category {
-    padding-top: 25px;
+    padding-top: 30px;
     width: 1000px;
     margin: 0 auto;
   }
